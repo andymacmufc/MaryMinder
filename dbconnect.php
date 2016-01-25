96e79fa46a4104ba1fbc13fcc163f65fb852fe85
PressStartGames <joey-hughes@hotmail.com>
2016-01-25 00:02:30 +0000

    Revert "A full update of files from server"
    This reverts commit c3da224e4d8374095bc0f60d2c1f44b266ccdf8f.

--- dbconnect.php
+++
@@ -1,9 +0,0 @@
-<?php
-		$db_host = "sql209.byethost10.com";
-		$db_name = "b10_16939691_users";
-		$username = "b10_16939691";
-		$password = "";
-
-		//@mysql_connect("$db_host","$username","$password") //or die ("no connection");
-		//@mysql_select_db("b10_16939691_a") //or die ("no database");
-	?>
